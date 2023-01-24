259ac2cb64ab6521f6d3b30f9a1ad08464af689f
ehddnr301 <dy950328@gmail.com>
2023-01-24 07:33:57 +0000

    Lab2

--- Lab2/README.md
+++ Lab2/README.md
@@ -4,14 +4,15 @@
 
 - Lab1에서는 CLI를 통해 deployment yaml file을 생성하고 배포합니다.
 
-- `prefect deployment build ./flow.py:flow_function -n ip-logging-pipeline -q instance1`
-- 위 커맨드로 생성된 yaml파일을 수정합니다.
+- `prefect deployment build ./flow.py:ETL -n stock-pipeline -q queue1` 으로 yaml파일을 생성합니다.
+- 위 커맨드로 생성된 yaml파일의 parameters 부분을 수정합니다.
 ```yaml
 parameters: {
-  "name": "HelloWorld"
+  "name": "Choonsik"
 }
 ```
-- `prefect deployment apply flow_function-deployment.yaml`
+
+- `prefect deployment apply ETL-deployment.yaml`
 - `prefect deployment ls`
-- `prefect deployment run My_first_pipeline/ip-logging-pipeline`
-- `prefect agent start -q 'instance1'`
+- `prefect deployment run ETL/stock-pipeline`
+- `prefect agent start -q 'queue1'`
--- Lab2/flow.py
+++ Lab2/flow.py
@@ -1,38 +1,59 @@
-from prefect import flow, task, get_run_logger
-import prefect
-import requests
 import sys
+from typing import List
+from datetime import date, timedelta
 
+import pandas as pd
+import yfinance as yf
+from sqlalchemy import create_engine
+from prefect import flow, task, get_run_logger
 
-@task
-def log_task(name):
+from settings import Settings
+
+@task(name="Extract")
+def extract(
+        start_date:str = str(date.today() - timedelta(days=365 * 1)),
+        end_date:str = str(date.today()),
+        tech_list:List[str] = ["AAPL", "GOOG", "MSFT", "AMZN"],
+    ):
+
+    # yfinance 데이터 download
+    df_list = [
+        yf.download(tech, start = start_date, end = end_date)
+        for tech in tech_list
+    ]
+
+    return df_list
+
+@task(name="Transform")
+def transform(
+        df_list,
+        company_list:List[str] = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+    ):
+    for company, com_name in zip(df_list, company_list):
+        company["company_name"] = com_name
+
+    df = pd.concat(df_list, axis=0)
+
+    df = df.reset_index(drop=False)
+
+    return df
+
+@task(name="Load")
+def load(df, table_name):
+    engine = create_engine(Settings.POSTGRES_HOST)
+
+    df.to_sql(table_name, con=engine, if_exists="append", index=False)
+
+@flow(name="ETL")
+def ETL(name):
     logger = get_run_logger()
-    logger.info("Hello %s!", name)
-    logger.info("Prefect Version = %s 🚀", prefect.__version__)
+    logger.info(f"{name} registered this task. ")
+    
+    df_list = extract()
+    df = transform(df_list)
+    load(df, "stock")
 
-
-@task(name="requests url")
-def request_url(url):
-    logger = get_run_logger()
-    response = requests.get(url)
-    status_code = response.status_code
-    if status_code < 400:
-        logger.info(status_code)
-    else:
-        logger.error(status_code)
-    return response.text
-
-
-@flow(name="My_first_pipeline")
-def flow_function(name):
-    log_task(name)
-    result = request_url("https://api.my-ip.io/ip")
-    logger = get_run_logger()
-    logger.info(result)
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     name = sys.argv[1]
-    flow_function(name)
-
-
+    ETL()
+    
