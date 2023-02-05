8d9f2299d67e82bb1dc23fd9831224f720257e66
ehddnr301 <dy950328@gmail.com>
2023-02-05 02:08:35 +0000

    Add Appendix

---
+++ Lab-Appendix/README.md
@@ -0,0 +1,7 @@
+# Appendix
+
+## ConcurrentTaskRunner VS SequentialTaskRunner
+
+- `python concurrent_flow.py`
+- `python sequential_flow.py`
+- 각각의 예제파일을 실행해보고 실행시간을 비교해봅니다.
---
+++ Lab-Appendix/sequential_flow.py
@@ -0,0 +1,22 @@
+import time
+from prefect import flow, task, get_run_logger
+from prefect.task_runners import SequentialTaskRunner
+
+@task(name="Download")
+def download(name):
+    time.sleep(5)
+    return f"Download {name}"
+
+@flow(name="Extract", task_runner=SequentialTaskRunner)
+def extract():
+    temp_list = []
+    for name in ["A", "B", "C", "D"]:
+        temp = download.submit(name)
+        temp_list.append(temp)
+    temp_list = [temp.result() for temp in temp_list]
+    return temp_list
+
+if __name__ == "__main__":
+    final_list = extract()
+    with open("./sequential.txt", 'w+') as file:
+        file.write('\n'.join(final_list)) 
