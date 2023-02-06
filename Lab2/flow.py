08da10aa30248f06f0c3589bf0134790ef48db85
ehddnr301 <dy950328@gmail.com>
2023-02-06 14:42:52 +0000

    Change Pykrx

--- Lab2/flow.py
+++ Lab2/flow.py
@@ -1,59 +1,64 @@
 import sys
+import random
 from typing import List
 from datetime import date, timedelta
 
 import pandas as pd
-import yfinance as yf
+from pykrx import stock
 from sqlalchemy import create_engine
 from prefect import flow, task, get_run_logger
 
 from settings import Settings
 
+
 @task(name="Extract")
 def extract(
-        start_date:str = str(date.today() - timedelta(days=365 * 1)),
-        end_date:str = str(date.today()),
-        tech_list:List[str] = ["AAPL", "GOOG", "MSFT", "AMZN"],
-    ):
+    start_date: str = str(date.today() - timedelta(days=365 * 1)),
+    end_date: str = str(date.today()),
+    ticker: str = "005930",
+):
 
-    # yfinance 데이터 download
-    df_list = [
-        yf.download(tech, start = start_date, end = end_date)
-        for tech in tech_list
-    ]
+    # Download Data
+    df = stock.get_market_ohlcv_by_date(start_date, end_date, ticker)
+    return df
 
-    return df_list
 
 @task(name="Transform")
-def transform(
-        df_list,
-        company_list:List[str] = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
-    ):
-    for company, com_name in zip(df_list, company_list):
-        company["company_name"] = com_name
+def transform(df, ticker: str = "005930"):
 
-    df = pd.concat(df_list, axis=0)
-
-    df = df.reset_index(drop=False)
+    # Preprocessing Data
+    df["Ticker"] = ticker
+    df = df.reset_index()
+    df.columns = Settings.COLUMNS
 
     return df
 
+
+def flipCoin():
+    return random.choice([True, True])
+
+
 @task(name="Load")
-def load(df, table_name):
+def load(df, table_name: str = "stock"):
     engine = create_engine(Settings.POSTGRES_HOST)
 
-    df.to_sql(table_name, con=engine, if_exists="append", index=False)
+    if flipCoin():
+        df.to_sql(table_name, con=engine, if_exists="append", index=False)
+        print("Data Insert Success")
+    else:
+        raise Exception("Data Insert Failed")
+
 
 @flow(name="ETL")
 def ETL(name):
     logger = get_run_logger()
     logger.info(f"{name} registered this task. ")
-    
+
     df_list = extract()
     df = transform(df_list)
     load(df, "stock")
 
+
 if __name__ == "__main__":
-    name = sys.argv[1]
+    name = sys.argv[1] if len(sys.argv) > 1 else "DefaultName"
     ETL(name)
-    
